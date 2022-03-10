fdb20fdc28e6db8506c8db7ebb0662cefdf59442
Mike <marino1820@gmail.com>
2022-03-09 23:40:16 -0500

    first local commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg'
+import './App.css'
+
+import Landing from './component/landing'
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<Landing />
+			</header>
+		</div>
+	)
+}
+
+export default App
